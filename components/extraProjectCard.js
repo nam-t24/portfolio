93b6cdbc7539c943e583873c65f9cdf68305ad52
nam-t24 <namtruong831@gmail.com>
2023-01-24 11:31:35 -0600

    UI upgrade to project timeline cards

--- components/extraProjectCard.js
+++ components/extraProjectCard.js
@@ -1,6 +1,6 @@
 const ExtraProjectCard = (props) => {
     return(
-        <div className="extraProjectBackground rounded-lg lg:w-[24rem] w-[20rem] lg:h-[18rem] h-[16rem] lg:my-4 md:my-3 my-2 lg:mr-8 mr-2 lg:p-6 p-4 flex flex-col justify-between">
+        <div className="extraProjectBackground rounded-lg lg:w-[24rem] w-[20rem] lg:h-[18rem] h-[16rem] lg:my-4 md:my-3 my-2 lg:mr-8 mr-2 lg:p-6 p-4 flex flex-col justify-between border-2 border-transparent hover:border-violet-500 extraProjectCard">
             <div>
                 <h1 className="flex items-center text-2xl font-bold text-[#7F62CF]">
                     {props.title}
