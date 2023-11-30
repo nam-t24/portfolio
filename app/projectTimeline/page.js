a4a0b849afddbf056def8e1cbaf3a60284491098
nam-t24 <namtruong831@gmail.com>
2023-11-30 16:31:32 -0600

    Stupid rookie mistake

--- app/projectTimeline/page.js
+++ app/projectTimeline/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from './N_black.png'
+import Logo from '../../public/assets/N_Black.png'
 import ExtraProjectCard from "../../components/extraProjectCard";
 
 const ProjectTimeline = () => {
