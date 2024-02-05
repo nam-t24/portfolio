77177ac0bb230621765f13078aaa451633b330f4
nam-t24 <namtruong831@gmail.com>
2024-02-05 17:50:02 -0600

    Fix skills section mobile view

--- components/skills.js
+++ components/skills.js
@@ -1,11 +1,11 @@
 const Skills = () => {
     return(
-        <div className="bg-[#F5F2FD] px-8 2xl:py-24 sm:py-20 py-16 text-slate-700">
+        <div className="bg-[#F5F2FD] px-8 2xl:py-24 py-16 text-slate-700">
             <div className="text-center text-neutral-800 2xl:text-6xl text-5xl font-semibold 2xl:mb-16 sm:mb-12 mb-10">Skills</div>
-            <div className="flex justify-center">
+            <div className="flex sm:justify-center justify-start">
                 <div className="flex flex-col items-start">
                     <div>
-                    <p className="w-32 text-neutral-800 2xl:text-2xl text-xl font-semibold mb-2">Languages</p>
+                    <p className="sm:w-32 w-auto text-neutral-800 2xl:text-2xl text-xl font-semibold mb-2">Languages</p>
                     <ul className="2xl:text-lg text-base">
                         <li>Java</li>
                         <li>Python</li>
@@ -19,8 +19,8 @@ const Skills = () => {
                     </ul>
                     </div>
                 </div>
-                <div className="2xl:mx-36 lg:mx-28 sm:mx-16 mx-5 flex flex-col items-center relative">
-                    <div className="2xl:px-36 lg:px-28 sm:px-16 px-6">
+                <div className="2xl:mx-36 lg:mx-28 sm:mx-16 mx-2 flex flex-col items-center relative">
+                    <div className="2xl:px-36 lg:px-28 sm:px-16 px-2">
                     <p className="text-neutral-800 2xl:text-2xl text-xl font-semibold mb-2">Frameworks</p>
                     <ul className="2xl:text-lg text-base">
                         <li>React</li>
@@ -34,7 +34,7 @@ const Skills = () => {
                 </div>
                 <div className="flex flex-col items-center">
                     <div>
-                    <p className="w-32 text-neutral-800 2xl:text-2xl text-xl font-semibold mb-2">Tools</p>
+                    <p className="sm:w-32 w-auto text-neutral-800 2xl:text-2xl text-xl font-semibold mb-2">Tools</p>
                     <ul className="2xl:text-lg text-base">
                         <li>Git</li>
                         <li>AWS</li>
